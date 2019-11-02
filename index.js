eb7997ebfd23c0a724ec0edae7e4d453aa3b6eef
Steve Elliott <steve@tegud.net>
2019-11-02 14:58:05 +0000

    test slack action

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const post = ({ webhookUrl, data }) => {
       path: url.pathname,
       method: "POST",
       headers: {
-        "Content-Type": "application/json; charset=utf-8",
+        "Content-Type": "application/x-www-form-urlencoded",
       }
     };
 
