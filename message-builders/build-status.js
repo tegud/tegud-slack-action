c5e29a7d555e074ed60e946cb016b30a71fc640a
Steve Elliott <steve@tegud.net>
2019-11-03 09:46:59 +0000

    lots of common logic to refactor

---
+++ message-builders/build-status.js
@@ -0,0 +1,32 @@
+const { getCommitContext } = require('../context/commit');
+const { getJobContext } = require('../context/job');
+const { getCommitFields } = require('./commit-fields');
+const { getViewInGithubButton } = require('./actions');
+
+const statusColors = {
+  success: 'good',
+  failed: 'danger',
+  cancelled: '#23AED9',
+  'in progress': 'warning',
+  unknown: '#999',
+};
+
+module.exports = () => {
+  const commitContext = getCommitContext();
+  const jobContext = getJobContext();
+
+  const { status } = jobContext;
+  const statusText = status === 'In Progress' ? 'Started' : `Finished - ${status}`;
+  
+  return {
+    title: `${commitContext.repository} ${jobContext.environment ? jobContext.environment : jobContext.workflow} Build ${statusText}`,
+    color: statusColors[status.toLowerCase()],
+    text: commitContext.message,
+    fields: [
+      ...getCommitFields(commitContext, jobContext),
+    ],
+    actions: [
+      getViewInGithubButton(commitContext, jobContext),
+    ],
+  };
+};
